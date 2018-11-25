39cbc64819ce65fc6a1abf614d8e309676c978a6
Dave Augustus <davetweetlive@gmail.com>
2018-11-25 16:19:17 +0530

    Added csv read function

--- csv_file.py
+++ csv_file.py
@@ -1,7 +1,12 @@
 import csv
+from csv import reader
 
-def csv_file():
-    pass
+def read_csv_file():
+    with open('record.csv') as csv_file:
+        csv_reader = reader(csv_file)
+        for row in csv_reader:
+            print(f'{row[0]} \t\t {row[1]} \t {row[2]} \t\t {row[3]}')
 
+read_csv_file()
 def update_csv():
     pass
